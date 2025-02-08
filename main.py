4bb16b2d6caa3d023820ecb384d01beec1a59357
AdityasArsenal <24adityapatil@gmail.com>
2025-02-09 00:08:15 +0530

    DB half way there
    the data inserting is taking a while and it has some errors
    more like bugs.

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from Environmental_handeler import get_env_data_from_xml
 from Governance_handeler import get_gov_data_from_xml
 from add_data_to_sheet import adder
 from scraper import download_xml_files
+from r1 import insertion
 
 # List of URLs to download
 company_names,file_names,urls = download_xml_files('All_xml_links24_25.xlsx')
 
-not_found_soc= []
-not_found_env= []
-not_found_gov= []
 
 social_kpi_names = [
     #"TrainingAndAwareness",
@@ -206,8 +204,8 @@ gov_kpi_names = [
     "MedianOfRemunerationOrSalaryOrWagesOfWorkers",
 ]
 
+company_counter = 0
 for company_name,file_name in zip(company_names,file_names):
-
     namespaces = { 
         'xbrli': 'http://www.xbrl.org/2003/instance',
         'in-capmkt': 'https://www.sebi.gov.in/xbrl/2024-04-30/in-capmkt'
@@ -215,27 +213,22 @@ for company_name,file_name in zip(company_names,file_names):
     
     tree = ET.parse(file_name)
     root = tree.getroot()
-
-    found_social_kpi_names,found_social_values,found_social_referance_unit,found_social_unit_refs,found_social_periods,found_social_decimals,not_found_social_kpi_names = get_social_data_from_xml(root, namespaces,social_kpi_names)
     
-    not_found_soc.extend(not_found_social_kpi_names)
-    adder(found_social_kpi_names,found_social_values,found_social_referance_unit,found_social_unit_refs,found_social_periods,found_social_decimals,not_found_social_kpi_names,company_name,"social info",urls)
-
-
+    found_social_kpi_names,found_social_values,found_social_referance_unit,found_social_unit_refs,found_social_periods,found_social_decimals,not_found_social_kpi_names = get_social_data_from_xml(root, namespaces,social_kpi_names)
     found_env_kpi_names,found_env_values,found_env_referance_unit,found_env_unit_refs,found_env_periods,found_env_decimals,not_found_env_kpi_names = get_env_data_from_xml(root, namespaces, env_kpi_names)
-
-    not_found_env.extend(not_found_env_kpi_names)
-    adder(found_env_kpi_names,found_env_values,found_env_referance_unit,found_env_unit_refs,found_env_periods,found_env_decimals,not_found_env_kpi_names,company_name,"env_res",urls)
-
-
     found_gov_kpi_names, found_gov_values, found_gov_referance_unit, found_gov_unit_refs, found_gov_periods, found_gov_decimals, not_found_gov_kpi_names =  get_gov_data_from_xml(root, namespaces, gov_kpi_names)
+    
 
-    not_found_gov.extend(not_found_gov_kpi_names)
+    adder(found_social_kpi_names,found_social_values,found_social_referance_unit,found_social_unit_refs,found_social_periods,found_social_decimals,not_found_social_kpi_names,company_name,"social info",urls)
+    adder(found_env_kpi_names,found_env_values,found_env_referance_unit,found_env_unit_refs,found_env_periods,found_env_decimals,not_found_env_kpi_names,company_name,"env_res",urls)
     adder(found_gov_kpi_names, found_gov_values, found_gov_referance_unit, found_gov_unit_refs, found_gov_periods, found_gov_decimals, not_found_gov_kpi_names,company_name,"gov info",urls)
 
-print(len(found_social_kpi_names))
-print(len(found_social_unit_refs))
-print(len(found_social_decimals))
-print(len(found_social_referance_unit))
 
+    insertion(company_name, "79aa665d-fdf5-4ad4-8550-727590914348", found_env_kpi_names,found_env_values,found_env_unit_refs,found_env_referance_unit,found_env_periods,found_env_decimals,company_counter)
+    insertion(company_name, "8f7b1de0-ff85-4d90-84af-61119dc77b39", found_gov_kpi_names,found_gov_values,found_gov_unit_refs,found_gov_referance_unit,found_gov_periods,found_gov_decimals,company_counter)
+    insertion(company_name, "f4f14038-ce0e-46ca-b453-d421a90e191b", found_social_kpi_names,found_social_values,found_social_unit_refs,found_social_referance_unit,found_social_periods,found_social_decimals,company_counter)
+
+
+
+    company_counter += 1
 
